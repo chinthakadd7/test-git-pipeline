9f4fe32268ad05abb3922f92262677dfd656ba7a
Chinthaka Dissanayake <143018090+chinthakadd7@users.noreply.github.com>
2025-10-31 20:22:33 +0530

    updated

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,7 +4,14 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                echo 'Hello World'
+                echo 'Hello World from Groovy!'
+            }
+        }
+
+        stage('Run Python Script') {
+            steps {
+                // This runs your Python file
+                sh 'python3 script.py'
             }
         }
     }
